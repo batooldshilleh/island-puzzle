14f1105e1feff31fe1a806bd928d6b3eff8d329d
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2021-11-06 08:58:25 +0200

    Add files via upload

---
+++ island_final_project_in_day_3.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+way = input("Are you going to the left or to the right ??\n").lower
+if way == "left":
+    method = input("Do you want to swim or will you wait for the boat? Enter a swim or wait\n ").lower
+    if method == "wait":
+        chose = input("Any door hides the treasure, write the name of the color to choose red, yellow, and blue\n").lower
+        if chose == "yellow" :
+            print("You've got the treasure, but don't let the money change you!!")
+        elif chose == "red" :
+            print("Oh no, the place is completely on fire, the fire of loss burned you ")
+        else :
+            print("You couldn't swim in the previous stage, so you will swim now. This is a room full of water ")
+    else :
+        print("Crocodiles will eat you up")
+else:
+    print("Bad start, you fell into the abyss")
+
+    
+        
